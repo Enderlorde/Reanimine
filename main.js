3e2ae29f96a0643164bf2b1800627cddb86b8eda
Enderlorde <ender.workpost@gmail.com>
2023-05-17 15:03:34 +0300

    added webpack and react

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload:path.join(__dirname, 'preload.js')
+        }
     })
 
-    window.loadFile("./index.html")
+    window.loadURL('localhost:3000')
 }
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
 
 app.whenReady().then(() => {
     createWindow()
